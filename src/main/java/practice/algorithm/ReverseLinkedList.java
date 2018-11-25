dd7da5b142b6df9443e9d9cf4d1b72da88f3d8cc
PRADEEP KAUSHAL <pradeep.kaushal@PRADEEPs-MacBook-Air.local>
2018-11-25 14:08:23 +0530

    adding reverse linked list

---
+++ src/main/java/practice/algorithm/ReverseLinkedList.java
@@ -0,0 +1,60 @@
+package practice.algorithm;
+
+public class ReverseLinkedList {
+
+    public static void main(String[] args) {
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        node1.next = node2;
+        node1.next.next = node3;
+        node1.next.next.next = node4;
+        System.out.println(node1);
+        Node reverse = reverseList(node1,null);
+        System.out.println(reverse);
+
+    }
+
+    public static Node reverse(Node node) {
+
+        Node current = node;
+        Node prev = null;
+        Node next;
+        while (current != null) {
+            next = current.next;
+            current.next=prev;
+            prev=current;
+            current = next;
+        }
+        node=prev;
+
+        return node;
+
+    }
+
+    public static Node reverseList(Node node,Node prev){
+        if(node==null){
+            return prev;
+        }
+        Node next=node.next;
+        node.next=prev;
+        prev=node;
+        return reverseList(next,prev);
+    }
+
+
+    static class Node {
+        public int data;
+        public Node next;
+
+        public Node(int data) {
+            this.data = data;
+        }
+
+        @Override
+        public String toString() {
+            return data + "->" + next;
+        }
+    }
+}
