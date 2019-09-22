15e1ce69c91737d9f1f605a51edf1e888183fa97
PRADEEP KAUSHAL <kaushal.pradeep89@gmail.com>
2019-09-22 09:10:10 +0530

    Adding the coded file

--- src/main/java/practice/algorithm/BinarySearch.java
+++ src/main/java/practice/algorithm/BinarySearch.java
@@ -1,4 +1,28 @@
 package practice.algorithm;
 
 public class BinarySearch {
+
+    public static void main(String[] args) {
+        int arr[] = {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+
+        System.out.println(findIndex(arr, 0, arr.length - 1));
+    }
+
+    public static int findIndex(int[] arr, int startIndex, int endIndex) {
+
+        if (startIndex <= endIndex) {
+            if (arr[startIndex] == 1) {
+                return startIndex;
+            }
+
+            int midIndex = (endIndex + startIndex) / 2;
+            if (arr[startIndex] == 0 && arr[midIndex] == 1) {
+                return findIndex(arr, startIndex, midIndex);
+            }
+            return findIndex(arr, midIndex + 1, endIndex);
+        }
+        return -1;
+    }
+
+
 }
---
+++ src/main/java/practice/algorithm/MaxSizeRect.java
@@ -0,0 +1,7 @@
+package practice.algorithm;
+
+public class MaxSizeRect {
+
+
+
+}
